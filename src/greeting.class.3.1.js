096f89bafdc44095112fb29483f3d9127b06b6da
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2022-12-11 13:07:40 -0300

    :tada: Initial commit

---
+++ src/greeting.class.3.1.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text } from "react-native-web";
+import { StyleSheet } from "react-native-web";
+
+export default function App() {
+  return (
+    <View>
+      <Text style={estilos}>Oi, Cauan</Text>
+    </View>
+  );
+}
+
+const estilos = StyleSheet.create({
+  vermelho: {
+    color: "red"
+  },
+  azul: {
+    color: "blue"
+  }
+})
